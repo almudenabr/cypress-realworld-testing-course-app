23758c739078e0e54b951653401ba3a83c8f98c7
Almudena Blanco <ablanco@sorint.it>
2023-08-29 17:34:45 +0200

    Add context to group related tests together and write tests for checking rediretion urls

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,17 +3,48 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("the h1 contains the correct text", () => {
-    // cy.get("[data-test='hero-heading']")
-    // the above is refactored into the below code:
-    cy.getByData("hero-heading")
-      .should("be.visible")
-      .contains("Testing Next.js Applications with Cypress")
+  context("Hero section", () => {
+    it("the h1 contains the correct text", () => {
+      // cy.get("[data-test='hero-heading']")
+      // the above is refactored into the below code:
+      cy.getByData("hero-heading")
+        .should("be.visible")
+        .contains("Testing Next.js Applications with Cypress")
+    })
+
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").should("be.visible").eq(0).contains("4 Courses")
+      cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
+      cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").should("be.visible").eq(0).contains("4 Courses")
-    cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
-    cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0")
+        .should("be.visible")
+        .find("a")
+        .contains("Get started")
+        .click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1")
+        .should("be.visible")
+        .find("a")
+        .contains("Get started")
+        .click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+
+    it("Course: Testing Cypress Fundamentals", () => {
+      cy.getByData("course-2")
+        .should("be.visible")
+        .find("a")
+        .contains("Get started")
+        .click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
   })
 })
