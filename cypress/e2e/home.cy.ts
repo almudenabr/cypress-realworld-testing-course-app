2aeedb93700ff95a266d5cfec57b1077a89a89cd
Almudena Blanco <ablanco@sorint.it>
2023-08-29 16:33:54 +0200

    Add tests using beforeEach, should be.visible, get data, contains txt and custom command

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("the h1 contains the correct text", () => {
+    // cy.get("[data-test='hero-heading']")
+    // the above is refactored into the below code:
+    cy.getByData("hero-heading")
+      .should("be.visible")
+      .contains("Testing Next.js Applications with Cypress")
+  })
+
+  it("the features on the homepage are correct", () => {
+    cy.get("dt").should("be.visible").eq(0).contains("4 Courses")
+    cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
+    cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
   })
 })
